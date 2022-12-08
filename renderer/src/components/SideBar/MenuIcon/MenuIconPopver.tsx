d08335a3364c053018df3d4cc09aee8fd5a7b073
shixinghao <245978782@qq.com>
2022-12-08 17:54:02 +0800

    chore

--- renderer/src/components/SideBar/MenuIcon/MenuIconPopver.tsx
+++ renderer/src/components/SideBar/MenuIcon/MenuIconPopver.tsx
@@ -4,36 +4,48 @@ import { Tooltip, Popover } from 'antd'
 import delay from 'lodash/delay'
 import './MenuIconPopver.less'
 
+interface WithPopoverPropsType extends MenuIconPropsType {
+  id: string | number
+  isPopover: boolean
+  subList?: WithPopoverPropsType[]
+}
+
+interface WithPopoverEventPropsType extends WithPopoverPropsType {
+  onRerenderLayout: Function
+}
+export type { WithPopoverPropsType }
+
 function withPopver(Component: React.ComponentType<MenuIconPropsType>) {
-  const WithPopver = (props: MenuIconPropsType) => {
-    console.log(props)
+  const WithPopver = <T extends WithPopoverEventPropsType>(props: T) => {
+    // console.log(props)
     const [isPopver, setIsPopver] = useState(false)
-    const handleMouseEnter = async (e: React.MouseEvent<HTMLDivElement>, menuIconProps: MenuIconPropsType) => {
+    const handleMouseEnter = async (e: React.MouseEvent<HTMLDivElement>, menuIconProps: T) => {
+      props.onRerenderLayout(menuIconProps, 'enter')
       console.log('鼠标移入', menuIconProps.name + '_' + menuIconProps.level)
       // 防止前一次移出未执行
-      if (isPopver) {
-        return
-      }
+      // if (isPopver) {
+      //   return
+      // }
       // 增加窗体宽度
-      await ipc.invoke<number>('win-change', { width: 70 })
+      // await ipc.invoke<number>('win-change', { width: 70 })
       // 窗体改变后，展示 popver
-      setIsPopver(true)
+      // setIsPopver(true)
       //   setTimeout(() => {
       //     debugger
       //   }, 1000)
     }
     const handleMouseLeave = async (e: React.MouseEvent<HTMLDivElement>, menuIconProps: MenuIconPropsType) => {
-      debugger
+      props.onRerenderLayout(menuIconProps, 'leave')
       console.log('鼠标移出', menuIconProps.name + '_' + menuIconProps.level)
-      if (!isPopver) {
-        return
-      }
-      delay(() => {
-        setIsPopver(false)
-      }, 400)
-      delay(async () => {
-        await ipc.invoke<number>('win-change', { width: -70 })
-      }, 800)
+      // if (!isPopver) {
+      //   return
+      // }
+      // delay(() => {
+      //   setIsPopver(false)
+      // }, 400)
+      // delay(async () => {
+      //   await ipc.invoke<number>('win-change', { width: -70 })
+      // }, 800)
     }
 
     let menuIconPopverJSX: JSX.Element = <></>
@@ -43,10 +55,11 @@ function withPopver(Component: React.ComponentType<MenuIconPropsType>) {
         <Popover
           placement="rightTop"
           trigger="hover"
-          open={isPopver}
+          open={props.isPopover}
           getPopupContainer={(triggerNode: HTMLElement) => triggerNode}
           content={
             <div className="more-menus">
+              {props.isPopover}
               {props.subMenuIcons.map((_props, index) => {
                 return (
                   <React.Fragment key={index}>
@@ -66,7 +79,7 @@ function withPopver(Component: React.ComponentType<MenuIconPropsType>) {
     // 单个元素
     else if (props.type == MenuType.SETTING_ICON || props.type == MenuType.CUSTOMER_ICON) {
       menuIconPopverJSX = (
-        <Tooltip placement="rightTop" open={isPopver} color="#2db7f5" title={props.name}>
+        <Tooltip placement="rightTop" open={props.isPopover} color="#2db7f5" title={props.name}>
           <div>
             <Component {...props} />
           </div>
--- renderer/src/components/SideBar/SideBar.tsx
+++ renderer/src/components/SideBar/SideBar.tsx
@@ -4,11 +4,10 @@ import './SideBar.less'
 import { FolderOutlined, SettingOutlined } from '@ant-design/icons'
 import { Tooltip, Popover } from 'antd'
 import MenuIcon, { MenuType, MenuIconPropsType } from './MenuIcon/MenuIcon'
-import { WithMenuIconPopver } from './MenuIcon/MenuIconPopver'
+import { WithMenuIconPopver, WithPopoverPropsType } from './MenuIcon/MenuIconPopver'
 
 function SideBar(props: { handleMouseOut: any }) {
-  const [count, setCount] = useState(0)
-  const [menus] = useState<MenuIconPropsType[]>([
+  const menuIcons: MenuIconPropsType[] = [
     { name: 'npm', level: 1, type: MenuType.CUSTOMER_ICON, icon: 'https://static.npmjs.com/b0f1a8318363185cc2ea6a40ac23eeb2.png', url: '' },
     { name: '语雀', level: 1, type: MenuType.CUSTOMER_ICON, icon: 'https://mdn.alipayobjects.com/huamei_0prmtq/afts/img/A*LmuNQ6eMk3gAAAAAAAAAAAAADvuFAQ/original', url: '' },
     { name: 'vite', level: 1, type: MenuType.CUSTOMER_ICON, icon: 'https://cn.vitejs.dev/logo.svg', url: '' },
@@ -25,26 +24,43 @@ function SideBar(props: { handleMouseOut: any }) {
       ],
     },
     { name: '语雀', level: 1, type: MenuType.MORE_ICON, icon: '', url: '', subMenuIcons: [] },
-  ])
-  //   const $body = document.getElementsByTagName('html')[0]
-  //   $body.addEventListener('mouseenter', function () {
-  //     console.log('mouseenter')
-  //     ipc.send('win-change', { type: 'in' })
-  //   })
-  //   $body.addEventListener('mouseleave', function () {
-  //     console.log('mouseleave')
-  //     ipc.send('win-change', { type: 'out' })
-  //   })
+  ]
+  const menuIconEvents: WithPopoverPropsType[] = menuIcons.map((menuIcon, index) => {
+    return {
+      isPopover: false,
+      ...menuIcon,
+      id: index + '-' + menuIcon.level,
+    }
+  })
+  const [list, setList] = useState<WithPopoverPropsType[]>(menuIconEvents)
+  const onRerenderLayout = async (activeMenuIcon: WithPopoverPropsType, mouseEnterOrLeave: string) => {
+    // console.log(activeMenuIcon, mouseEnterOrLeave)
+    if (mouseEnterOrLeave == 'enter') {
+      await ipc.invoke<number>('win-change', { width: 70 })
+    }
+    setList(
+      list.map((item) => {
+        return { ...item, isPopover: mouseEnterOrLeave == 'enter' && item.id == activeMenuIcon.id }
+      })
+    )
+    if (mouseEnterOrLeave == 'leave') {
+      await ipc.invoke<number>('win-change', { width: -70 })
+    }
+  }
 
   return (
     <div className="side-bar" onMouseLeave={props.handleMouseOut}>
       <div className="icon-bar">
         {/* TODO: 不合规的 schema 需要 filter */}
-        {menus.map((menu, index) => {
-          return <WithMenuIconPopver {...menu} key={index} />
+        {list.map((menu) => {
+          return (
+            <React.Fragment key={menu.id}>
+              <WithMenuIconPopver {...menu} onRerenderLayout={onRerenderLayout} />
+            </React.Fragment>
+          )
         })}
       </div>
-      <WithMenuIconPopver name={'设置'} type={MenuType.SETTING_ICON} level={0} />
+      <WithMenuIconPopver id="setting" onRerenderLayout={onRerenderLayout} isPopover={false} name={'设置'} type={MenuType.SETTING_ICON} level={0} />
     </div>
   )
 }
