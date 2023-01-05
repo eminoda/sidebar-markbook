0888d34179b4dd6f8e5321e7c0073092e95f6b94
eminoda <245978782@qq.com>
2023-01-06 00:35:52 +0800

    feat: todo image

--- main/windows/todoWindow.js
+++ main/windows/todoWindow.js
@@ -1,28 +1,38 @@
-const { app, BrowserWindow, screen } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, screen } = require('electron');
+const path = require('path');
+
+const createWindow = () => {};
 module.exports = {
   open: () => {
-    console.log(BrowserWindow.getAllWindows().map((win) => win.getTitle()))
-    const rect = {
-      width: 360,
-      height: 440,
+    const todoWin = BrowserWindow.getAllWindows().find((win) => win.getTitle() == 'todo');
+    if (todoWin) {
+      todoWin.focus();
+    } else {
+      const rect = {
+        width: 360,
+        height: 440,
+      };
+      const primaryDisplay = screen.getPrimaryDisplay();
+      const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize;
+      const win = new BrowserWindow({
+        width: rect.width,
+        height: rect.height,
+        frame: false,
+        show: false,
+        transparent: true,
+        x: parseInt(screenWidth / 2 - rect.width / 2),
+        y: parseInt((screenHeight - rect.height) / 2),
+        movable: true,
+        webPreferences: {
+          preload: path.join(__dirname, '../preload.js'),
+          devTools: true,
+        },
+      });
+      win.loadURL('http://127.0.0.1:5173/todo');
+      win.once('ready-to-show', () => {
+        win.setTitle('todo');
+        win.show();
+      });
     }
-    const primaryDisplay = screen.getPrimaryDisplay()
-    const { width: screenWidth, height: screenHeight } = primaryDisplay.workAreaSize
-    const win = new BrowserWindow({
-      width: rect.width,
-      height: rect.height,
-      frame: false,
-      x: parseInt(screenWidth / 2 - rect.width / 2),
-      y: parseInt((screenHeight - rect.height) / 2),
-      // resizable: false,
-      webPreferences: {
-        preload: path.join(__dirname, '../preload.js'),
-        devTools: true,
-      },
-    })
-    win.loadURL('http://127.0.0.1:5173/todo')
-    win.setTitle('todo')
-    console.log(win.getTitle())
   },
-}
+};
--- renderer/src/views/Todo/Todo.tsx
+++ renderer/src/views/Todo/Todo.tsx
@@ -1,5 +1,48 @@
+import "./Todo.less"
 import React from 'react'
+import { Checkbox } from 'antd';
+import { PlusOutlined, CloseOutlined } from '@ant-design/icons'
+import test from "./test.jpg"
+
+const colors = [
+  { bar: '#434343', line: '' },
+  { bar: '#1677ff', line: '' },
+  { bar: '#eb2f96', line: '' },
+  { bar: '#722ed1', line: '' },
+  { bar: '#13c2c2', line: '' },
+  { bar: '#52c41a', line: '' },
+  { bar: '#fadb14', line: '' },
+]
 const Todo = (props: {}) => {
-  return <div>1</div>
+  return <div className="todo">
+    {/* toolbar */}
+    <div className="todo-control">
+      <PlusOutlined /><CloseOutlined />
+    </div>
+    {/* 日期任务 */}
+    <div className="todo-list">
+      {colors.map(color => {
+        const borderStyle = {
+          border: `1px thin ${color.bar}`
+        }
+        const barStyle = {
+          'backgroundColor': `${color.bar}`
+        }
+        const bgStyle = {
+          // WebkitMaskImage: `-webkit-gradient(linear, right bottom, 10% 100%, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))`
+          WebkitMaskImage: `-webkit-gradient(linear, 80% 50%, 10% 50%, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))`
+        }
+        return (<div className="todo-list--item" style={borderStyle}>
+          <div className="todo-list--item-bar" style={barStyle}></div>
+          <div className="todo-list--item-title">Vaundy</div>
+          <div className="todo-list--item-bg">
+            <img src={test} alt="" style={bgStyle} />
+          </div>
+          <div className="todo-list--item-content"><Checkbox>踊り子</Checkbox></div>
+          <div className="todo-list--item-content"><Checkbox>怪獣の花唄</Checkbox></div>
+        </div>)
+      })}
+    </div>
+  </div >
 }
 export default Todo
